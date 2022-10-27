add9858617c965aab6d5e220d74c3cdf83d6ea06
Eleonore <eleonore.jens@gmail.com>
2022-10-27 22:58:52 +0200

    ajout css

---
+++ cube_animation/cube_animation.js
@@ -0,0 +1,52 @@
+let timer = setInterval(move, 10);
+let pos = 0;
+let ball = document.getElementById("ball");
+
+function move() {
+  if (pos >= 270) {
+    // the ball reached the end of the container (because 200px-50px = 150px)
+    clearInterval(timer);
+  } else {
+    pos += 1;
+    // right, bottom, left, top
+    ball.style.right = pos + "px"; // déplace la boite par la gauche, son coté gauche change de position
+    ball.style.top = pos + "px"; // déplace la boite par le haut, son coté haut change de position
+  }
+}
+
+document.getElementById("valider").addEventListener("click", (event) => {
+  let horizontale_value = document.getElementById("position1-select").value;
+  let verticale_value = document.getElementById("position2-select").value;
+
+  let timer = setInterval(move, 10);
+  let pos = 0;
+  let ball = document.getElementById("ball");
+
+  function move() {
+    if (pos >= 270) {
+      clearInterval(timer);
+    } else {
+      pos += 1;
+      if (horizontale_value == "right") {
+        ball.style.right = pos + "px";
+      }
+      else {
+        ball.style.left = pos + "px";
+      }
+      if (verticale_value == "top") {
+        ball.style.top = pos + "px";
+      }
+      ball.style.bottom = pos + "px";
+    }
+  }
+
+  if (horizontale_value === "left" && verticale_value === "bottom") {
+    document.getElementById('valider').style.display = "none";
+    setTimeout(() => {
+      return window.location.assign("/index_nasa_video.html");
+    }, 6000);
+  }
+  else {
+    $('#myModal').modal('show');
+  }
+});
---
+++ nasa_video/nasa_video.js
@@ -0,0 +1,53 @@
+document.getElementById('valider').addEventListener("click", event => {
+    let api_key = document.getElementById('api_key').value;
+
+    if (api_key === "DEMO_KEY") {
+        let array_request = ["https://api.nasa.gov/planetary/apod?api_key=3mpbloKSNnuBe6UpTH6LuvEpHLuMstO6NcviGMEj&date=2018-06-18",
+            "https://api.nasa.gov/planetary/apod?api_key=3mpbloKSNnuBe6UpTH6LuvEpHLuMstO6NcviGMEj&date=2020-09-29",
+            "https://api.nasa.gov/planetary/apod?api_key=3mpbloKSNnuBe6UpTH6LuvEpHLuMstO6NcviGMEj&date=2017-05-31"]
+        let random_index = Math.round(Math.random() * (array_request.length - 1));
+
+        //requête à l'API et récupération de la réponse en JSON - remplace xhr
+        fetch(array_request[random_index])
+            .then(response => {
+                if (response.status == 200) return response.json();
+                else console.error(`Error API : status ${response.status}`);
+            }
+            )
+            .then(json => showApi(json));
+
+        // disparition du bouton pour ne plus pouvoir faire des requêtes à l'API
+        // point d'amélioration: faire un removeEventListener
+        document.getElementById('valider').style.display = "none";
+
+        // permet le passage après un certain délai à la page index qui va lancer le prochain jeu
+        let delay = 25000;
+        if (random_index === 2){
+            delay = 45000;
+        }
+        console.log(delay);
+        setTimeout(() => {
+            // mettre à jour l'index
+            return window.location.assign("/index_color_picker.html");
+        }, delay);
+    }
+    else {
+        $('#myModal').modal('show');
+    }
+});
+
+// fonction qui permet l'affichage de la réponse API dans le DOM 
+const showApi = function (reponse) {
+    let div = document.getElementById('resultat');
+    let iframe = document.createElement('iframe');
+    let title = document.createElement('h3');
+    let author = document.createElement('p');
+    iframe.src = reponse.url;
+    iframe.width = "600";
+    iframe.height = "420";
+    title.innerHTML = reponse.title;
+    author.innerHTML = `© ${reponse.copyright}`;
+    div.appendChild(iframe);
+    div.appendChild(title);
+    div.appendChild(author);
+};
